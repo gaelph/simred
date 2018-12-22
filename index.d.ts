a0c1977edcabc58a3483fe28e4dc8276b9d13e26
gaelph <contact@gael-philippe.fr>
2018-12-23 00:05:55 +0100

    Improve type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,36 @@
-export namespace Simred {
-  interface ReducerDescriptor<T> {
-    actions: Reducer<T>,
-    state?: any
-  }
+interface ReducerDescriptor<T> {
+  actions: Reducer<T>,
+  state?: any
+}
 
-  type Reducer<T> = {
-    [K in keyof T]: (...args: any[]) => void
-  }
+type Reducer<T> = {
+  [K in keyof T]: (...args: any[]) => void
+}
 
-  type ListenerFunction = (state: any) => void
-  type MiddlewareFunction = (actionName: string, action: Payload, next: () => void) => void
+type Payload = any
 
-  export interface Store {
-    getState(): any
-    getActions(): any
-    subscribe(listener: ListenerFunction): void
-    addMiddleware(middleware: MiddlewareFunction): void
-  }
+type ListenerFunction = (state: any) => void
+export type MiddlewareFunction = (actionName: string, action: Payload, next: () => void) => void
+
+export interface Store<T> {
+  getState(): T
+  getActions(): any
+  subscribe(listener: ListenerFunction): void
+  addMiddleware(middleware: MiddlewareFunction): void
+}
+
+// export namespace Simred {
   
   export function createReducer<T>(reducer: T, initialState?: any): ReducerDescriptor<T>
 
   export function getState(): any
   export function getActions(): any
+// }
+
+export namespace Simred {
+  export function createStore(reducers: any, initialState?: any): Store<any>
 }
 
-export function createStore(reducers: any, initialState?: any): Store
+declare const simred: typeof Simred
+
+export default simred
